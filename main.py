96380f01a8ced67e071aae8aa29d5c05ec82570c
Leonidzxc <130896334+Leonidzxc@users.noreply.github.com>
2023-04-23 15:00:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,20 @@ class GameSprite(sprite.Sprite):
 # класс главного игрока
 class Player(GameSprite):
     # метод для управления спрайтом стрелками клавиатуры
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
 
 
 # Игровая сцена:
@@ -41,11 +49,20 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 
+# создания мяча и ракетки
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)  # при созданни спрайта добавляется еще два параметра
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+
 # флаги отвечающие за состояние игры
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
+speed_x = 2
+speed_y = 2
 
 while game:
     for e in event.get():
@@ -53,6 +70,25 @@ while game:
             game = False
     if not finish:
         window.fill(back)
+
+        if ball.rect.y <= 0 or ball.rect.y >= win_height - 50:
+            speed_y *= -1
+
+        if sprite.collide_rect(ball, racket1) or sprite.collide_rect(ball, racket2):
+            speed_x *= -1
+
+        if ball.rect.x <= 0 or ball.rect.x >= win_width - 50:
+            finish = True
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
 
     display.update()
     clock.tick(FPS)
